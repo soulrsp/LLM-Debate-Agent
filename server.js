b6395d5acd7b7eaff1275414ba439843af49bec8
soulrsp <soulrsp@gmail.com>
2026-08-04 16:43:42 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // Provider Configurations with Official Tested Model IDs
 const PROVIDER_CONFIGS = {
